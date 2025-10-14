4cd49cca78fc8d838ceb9e6080b9634e35188cdc
heroson02 <heroson0925@gmail.com>
2025-10-15 04:34:36 +0900

    feat : moricash 페이지 추가

--- src/app/(site)/user-dashboard/navigation.ts
+++ src/app/(site)/user-dashboard/navigation.ts
@@ -5,11 +5,19 @@ export const navItems = [
   { href: '/user-dashboard/follow-list', label: '팔로우하는 작가' },
   { href: '/user-dashboard/funding-list', label: '참여한 펀딩 목록' },
   {
-    href: '',
+    href: 'artist',
     label: '작가 신청',
     subItems: [
       { href: '/user-dashboard/apply-author/apply', label: '신청하기' },
       { href: '/user-dashboard/apply-author/history', label: '신청 내역' },
+    ],
+  },
+  {
+    href: 'mori-cash',
+    label: '모리 캐시',
+    subItems: [
+      { href: '/user-dashboard/mori-cash/charge', label: '캐시 충전' },
+      { href: '/user-dashboard/mori-cash/history', label: '충전 / 사용 내역' },
     ],
   },
 ];
